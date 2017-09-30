d3eface08693db3ee2a6cbf609eb837cc17e2e8e
AimainaHito <AimainaHito@users.noreply.github.com>
2017-09-30 13:26:47 +0200

    Streamlined imports

--- src/exmoji/__init__.py
+++ src/exmoji/__init__.py
@@ -1,2 +1,2 @@
 from exmoji.processing import AspectDatalist, AspectDatalistBase, SentimentDatalist, IOB_Type
-from exmoji.nn import Model, IOBModel, AspectPolarityModel, train
+from exmoji.nn import Model, IOBModel, AspectPolarityModel, train, Mode
